bb2fe02aa225650f64e134323d8b3de9e7230d48
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-16 10:33:42 -0500

    Displaying query data
    server.py: adjusted route /  to call a query, store it in a variable called 'friends', and created another variable that will be rendered with index.html called 'all_friends' that will hold the results of the query.
    index.html:  Removed hard coded information and replaced with a for loop using jinja2 to loop through the array of dictionaries given from the all_friends variable and display the information accordingly.

--- server.py
+++ server.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 mysql = MySQLConnector(app,'friendsdb')
 @app.route('/')
 def index():
-    friends = mysql.query_db("SELECT * FROM friends")
-    print friends
-    return render_template('index.html')
+    query = "SELECT * FROM friends"                           # define your query
+    friends = mysql.query_db(query)                           # run query with query_db()
+    return render_template('index.html', all_friends=friends) # pass data to our template
+
 
 @app.route('/friends', methods=['POST'])
 def create():
